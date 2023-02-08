d51c8941c0b56b64e1dc4886318b1fbbc6ac99a5
Adham Omari <adhamomarics@gmail.com>
2023-02-08 21:31:04 +0300

    add

--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,187 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button';
-import { Slider, Typography } from '@mui/material';
+import { useState, useEffect, use } from 'react'
+import Button from '@mui/material/Button'
+import { Slider, Typography } from '@mui/material'
 
 export default function Home() {
-  const [color, setColor] = useState(0);
-  const [sticker, setSticker] = useState("0");
-  const [X, setX] = useState(320);
+  const [color, setColor] = useState(0)
+  const [sticker, setSticker] = useState('')
+  const [X, setX] = useState(59);
+  const [buttonColor, setButtonColor] = useState(0);
 
-  const [Y, setY] = useState(27);
+
+  const [Y, setY] = useState(0)
+
+  const [car, setCar] = useState('')
+  const [paint, setPaint] = useState('')
+
+
+
+
+
 
 
   return (
     <>
-
       <main>
-
         <div className='wrap '>
-          <div className='flex'>
-            {/* <input type="range" id="volume" name="volume"
-              min="0" max="360" onChange={(e) => setColor(e.target.value)} style={{ background: "rgb(33 52 190)" }} /> */}
-            <Typography id="linear-slider" gutterBottom square  >
+          <div className='flex '>
+            <div className='color-picker'>
 
-              <Slider
-                sx={{
-                  width: 130,
-                  color: `${color}`,
-                  '& .MuiSlider-thumb': {
-                    borderRadius: '23px',
 
-                  },
+              <form onChange={e => setColor(e.target.value)}
+              >
+                <div class="form-group" >
+                  <label for="formControlRange"></label>
+                  <input type="range" class="form-control-range color-picker progress  "  min="0" max="360"/>
+
+                </div>
+              </form>
+            </div>
+
+            <div className="button-change">
+              <Button className='btn-color-change'
+                onClick={() =>setColor(0)}
+                variant=''
+                style={{ background: 'red' }}
+              ></Button>
+              <Button className='btn-color-change'
+                onClick={() => setColor(138)}
+                variant=''
+                style={{ background: '#005500' }}
+              ></Button>
+              <Button className='btn-color-change'
+                onClick={() => setColor(244)}
+                variant=''
+                style={{ background: '#0b1bcb' }}
+              ></Button>
+              <Button className='btn-color-change'
+                onClick={() => setColor(322)}
+                variant=''
+                style={{ background: '#c30576' }}
+              ></Button>
+            </div>
+
+            <div className='change-car'>
+              <Button
+                onClick={() => {
+                  setCar('/car3.png'), setPaint('/paintf.png')
                 }}
-                min={0}
-                step={1}
-                max={360}
-                onChange={(e) => setColor(e.target.value)}
-                valueLabelDisplay="auto"
-                aria-labelledby="non-linear-slider"
-              />
-            </Typography>
+                variant=''
+                style={{ background: 'red' }}
+              >
+                Dodge{' '}
+              </Button>
+    
+              <Button
+                onClick={() => {
+                  setCar('/car.png'), setPaint('/paint.png')
+                }}
+                variant=''
+                style={{ background: 'red' }}
+              >
+                Jaguar{' '}
+              </Button>
+            </div>
 
+            <div className='sticker-picker'>
+              <Button
+                onClick={() => setSticker(1)}
+                variant=''
+                className='btn-color'
+                style={{
+                  background: 'url(/sticker1.png)no-repeat ',
+                  backgroundSize: 'contain'
+                }}
+              ></Button>
+              <Button
+                onClick={() => setSticker(2)}
+                variant=''
+                className='btn-color'
+                style={{
+                  background: 'url(/sticker2.png)no-repeat ',
+                  backgroundSize: 'contain'
+                }}
+              ></Button>
+              <Button
+                onClick={() => setSticker(3)}
+                variant=''
+                className='btn-color'
+                style={{
+                  background: 'url(/sticker3.png)no-repeat ',
+                  backgroundSize: 'contain'
+                }}
+              ></Button>
+              <Button
+                onClick={() => setSticker(4)}
+                variant=''
+                className='btn-color'
+                style={{
+                  background: 'url(/sticker4.png)no-repeat ',
+                  backgroundSize: 'contain'
+                }}
+              ></Button>
+              <Button
+                onClick={() => setSticker(5)}
+                variant=''
+                className='btn-color'
+                style={{
+                  background: 'url(/sticker5.png)no-repeat ',
+                  backgroundSize: 'contain'
+                }}
+              ></Button>
 
-            <Button onClick={() => setColor(0)} variant="" style={{ background: "red" }}></Button>
-            <Button onClick={() => setColor(138)} variant="" style={{ background: "#005500" }}></Button>
-            <Button onClick={() => setColor(244)} variant="" style={{ background: "#0b1bcb" }}></Button>
-            <Button onClick={() => setColor(322)} variant="" style={{ background: "#c30576" }}></Button>
-            <Button onClick={() => setSticker(1)} variant="" className='btn-color' style={{ background: "url(/sticker1.png)no-repeat ", backgroundSize: "contain" }}></Button>
-            <Button onClick={() => setSticker(2)} variant="" className='btn-color' style={{ background: "url(/sticker2.png)no-repeat ", backgroundSize: "contain" }}></Button>
-            <Button onClick={() => setSticker(3)} variant="" className='btn-color' style={{ background: "url(/sticker3.png)no-repeat ", backgroundSize: "contain" }}></Button>
-            <Button onClick={() => setSticker(4)} variant="" className='btn-color' style={{ background: "url(/sticker4.png)no-repeat ", backgroundSize: "contain" }}></Button>
-            <Button onClick={() => setSticker(5)} variant="" className='btn-color' style={{ background: "url(/sticker5.png)no-repeat ", backgroundSize: "contain" }}></Button>
+              <div className='sticker-position'>
+              <form onChange={e => setX(e.target.value)}
+              >
+                <div class="form-group" >
+                  <label for="formControlRange"></label>
+                  <input type="range" class="form-control-range  progress " min="47" max="94" />
+                </div>
+              </form>
+              <form onChange={e => setY(e.target.value)}
+              >
+                <div class="form-group" >
+                  <label for="formControlRange"></label>
+                  <input type="range" class="form-control-range  progress " min="0" max="455"  />
+                </div>
+              </form>
 
-
-            <Typography id="non-linear-slider" gutterBottom>
-
-            </Typography>
-            <Slider
-
-              min={5}
-              step={1}
-              max={220}
-
-              onChange={(e) => setX(e.target.value)}
-              valueLabelDisplay="auto"
-              aria-labelledby="non-linear-slider"
-            />
-            <Typography id="non-linear-slider" gutterBottom>
-
-            </Typography>
-            <Slider
-
-              min={5}
-              step={1}
-              max={220}
-
-              onChange={(e) => setY(e.target.value)}
-              valueLabelDisplay="auto"
-              aria-labelledby="non-linear-slider"
-            />
-
-
-
+              </div>
+            </div>
           </div>
 
-
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <div class="thumbnail">
-                  <div className='car-wrapper' >
-
-                    <div className='image-car relative left-0 top-0  ' ></div>
-                    <div className='image-paint absolute  top-0  ' style={{ filter: `hue-rotate(${color}deg)` }} ></div>
-
+          <div class='container'>
+            <div class='row'>
+              <div class='col'>
+                <div class='thumbnail'>
+                  <div className='car-wrapper  left-0 top-0'>
+                    <div
+                      className='image-car  left-0 top-0'
+                      style={{ backgroundImage: `url(${car})` }}
+                    ></div>
+                    <div
+                      className='image-paint absolute  top-0  '
+                      style={{
+                        filter: `hue-rotate(${color}deg)`,
+                        backgroundImage: `url(${paint})`
+                      }}
+                    ></div>
+                    <div
+                      className='image-sticker absolute     '
+                      style={{
+                        backgroundImage: `url(/sticker${sticker}.png)`,
+                        top: `${X}px`,
+                        left: ` ${Y}px `,
+                        WebkitMaskImage: `url${paint}`
+                      }}
+                    ></div>
                   </div>
-                  <div className='image-sticker absolute   w-width h-height  ' style={{ backgroundImage: `url(/sticker${sticker}.png)`, left: `${900}px`, top: `${Y}px}` }}></div>
-
                 </div>
               </div>
             </div>
-            </div>
-            </div>
-
-
-
-          </main>
-        </>
-        )
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
