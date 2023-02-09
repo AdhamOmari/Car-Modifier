118041e6a60d4f88b9c2103c28370eb8cf5b146c
Adham Omari <adhamomarics@gmail.com>
2023-02-09 19:55:02 +0300

    add

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
               >
                 <div class="form-group" >
                   <label for="formControlRange"></label>
-                  <input type="range" class="form-control-range  progress " min="0" max="500"  />
+                  <input type="range" class="form-control-range  progress " min="0" max="585"  />
                 </div>
               </form>
 
